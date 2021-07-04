6a67ce5ecb1385bb26cbc71acacde9b0bce40607
Rohit kadam <rkrohit123@gmail.com>
2021-07-04 23:13:32 +0530

    modified main along with css and js script

--- MyResume/assets/js/main.js
+++ MyResume/assets/js/main.js
@@ -188,4 +188,23 @@
         items: 1
     });
 
+    //Portfolio modals
+    document.addEventListener('DOMContentLoaded', function(e) {
+        let field = document.querySelector('.field');
+        let input = document.querySelector('input');
+        let copyBtn = document.querySelector('.field button');
+
+        copyBtn.onclick = () => {
+            input.select();
+            if (document.execCommand("copy")) {
+                field.classList.add('active');
+                copyBtn.innerText = 'Copied';
+                setTimeout(() => {
+                    field.classList.remove('active');
+                    copyBtn.innerText = 'Copy';
+                }, 3500)
+            }
+        }
+    })
+
 })(jQuery);
